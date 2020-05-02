2670be1e1671311f9c052cb44ed0fbf4e6dd42e9
kroyedapper <kroyedapper@gmail.com>
2020-05-02 20:37:55 +0100

    Second day of coding in Python. Finding missing number in python

---
+++ missing_number.py
@@ -0,0 +1,8 @@
+def missing_numbers(num_list):
+        start = num_list[0]
+        end= num_list[-1]
+        return set(range(start,end + 1)).difference(num_list)
+
+print(missing_numbers([1, 2, 3, 4, 6, 7, 10]))
+print(missing_numbers([10, 11, 12, 14, 17]))
+print(missing_numbers([5, 10, 15, 20, 25, 30]))
